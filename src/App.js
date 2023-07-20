2f68003134d76431ce1aceb653b8dc6d04ea33d6
Yesenia L <ylopezcr123@gmail.com>
2023-07-20 11:21:34 -0400

    loading and error

--- src/App.js
+++ src/App.js
@@ -4,26 +4,45 @@ import Nav from "./components/nav";
 import Update from "./components/Update";
 import Feed from "./components/Feed";
 import Hobbies from "./components/Hobbies";
+import Loading from "./components/Loading";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [err, setErr] = useState('');
 
   const API_URL = "http://localhost:9000";
   useEffect(() => {
     console.log("app rendered");
     async function fetchData() {
+      try{
+      setLoading(true);
       const res = await fetch(`${API_URL}/users`);
       const json = await res.json();
       console.log(json, "json");
-      const { data } = json;
+      const { data,err} = json;
+      data.split(' ')
       console.log(data, "data");
-      setUsers(data);
+      if(res.ok) {
+        setUsers(data);
+        setLoading(false)
+      }else {
+        setErr(err)
+        setLoading(false)
+      }
+    }catch (err){
+      console.log(`err : ${err.message}`)
+      setLoading(false);
+      setErr(err.message)
+    }
     }
     fetchData();
   }, []);
 
+
   return (
     <div>
+{ loading? <Loading /> : <div>
       <Nav />
       <div className="home">
         <Update />
@@ -31,6 +50,8 @@ function App() {
       </div>
       <h1>LIVE:</h1>
       <Feed users={users} />
+      </div>
+    }
     </div>
   );
 }
