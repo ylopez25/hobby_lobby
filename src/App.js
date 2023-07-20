743688630dbcac1e2f5f265d47561d3966977721
Yesenia L <ylopezcr123@gmail.com>
2023-07-20 11:40:34 -0400

    fixed error bug

--- src/App.js
+++ src/App.js
@@ -9,51 +9,57 @@ import Loading from "./components/Loading";
 function App() {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [err, setErr] = useState('');
+  const [err, setErr] = useState("");
 
   const API_URL = "http://localhost:9000";
   useEffect(() => {
     console.log("app rendered");
     async function fetchData() {
-      try{
-      setLoading(true);
-      const res = await fetch(`${API_URL}/users`);
-      const json = await res.json();
-      console.log(json, "json");
-      const { data,err} = json;
-      // data.split(' ')
-      console.log(data, "data");
-      if(res.ok) {
-        setUsers(data);
-        setLoading(false)
-      }else {
-        setErr(err)
-        setLoading(false)
+      try {
+        setErr("");
+        setLoading(true);
+        const res = await fetch(`${API_URL}/users`);
+        const json = await res.json();
+        console.log(json, "json");
+        const { data, err } = json;
+        data.split(" ");
+        console.log(data, "data");
+        if (res.ok) {
+          setUsers(data);
+          setLoading(false);
+        } else {
+          setErr(err);
+          setLoading(false);
+        }
+      } catch (err) {
+        setLoading(false);
+        setErr(`err: ${err.message}`);
       }
-    }catch (err){
-      console.log(`err : ${err.message}`)
-      setLoading(false);
-      setErr(err.message)
-    }
     }
     fetchData();
   }, []);
 
-
-  return (
-    <div>
-{ loading? <Loading /> : <div>
-      <Nav />
-      <div className="home">
-        <Update />
-        <Hobbies users={users} />
-      </div>
-      <h1>LIVE:</h1>
-      <Feed users={users} />
-      </div>
+  const renderContent = () => {
+    if (loading) {
+      return <Loading />;
+    } else if (err) {
+      return err;
+    } else {
+      return (
+        <div>
+          <Nav />
+          <div className="home">
+            <Update />
+            <Hobbies users={users} />
+          </div>
+          <h1>LIVE:</h1>
+          <Feed users={users} />
+        </div>
+      );
     }
-    </div>
-  );
+  };
+
+  return <div>{renderContent()}</div>;
 }
 
 export default App;
