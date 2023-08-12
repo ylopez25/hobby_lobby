b9bb0f0c738285c35f839c245104d5b35c4111c8
Yesenia L <ylopezcr123@gmail.com>
2023-08-11 20:36:21 -0400

    gallery

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -2,9 +2,13 @@ import React from "react";
 import "./Feed.css";
 
 export default function Feed({ users }) {
+  console.log(users.photodump, "users");
+
   return (
     <div className="users">
       {users.map((user) => {
+        const photos = user.photodump;
+        console.log(user.photodump, "user");
         return (
           <div className="userCard">
             <div className="userinfo">
@@ -13,14 +17,15 @@ export default function Feed({ users }) {
               <p>{user.city}</p>
               <p>follow</p>
             </div>
-            <div className="hobby_pics">
-              <div><h2>Profile pics</h2></div>
-              <p>{user.grades}</p>
-              <img src={user.pic} alt='img'/>
-              <img src={user.pic} alt='img'/>
-              <img src={user.pic} alt='img'/>
-              <img src={user.pic} alt='img'/>
-              <img src={user.pic} alt='img'/> <img src={user.pic} alt='img'/> <img src={user.pic} alt='img'/> <img src={user.pic} alt='img'/>
+            <div className="hobby">
+              <div>
+                <h2>Profile pics</h2>
+              </div>
+              <div className='hobby_pics'>
+              {photos?.map((img, id) => (
+                <img key={id} src={img} alt="img" />
+              ))}
+              </div>
             </div>
           </div>
         );
