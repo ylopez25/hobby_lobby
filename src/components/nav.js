719996ffd3186801dd0e15e924a4b018944ea6fd
Yesenia L <ylopezcr123@gmail.com>
2023-09-24 23:56:43 -0400

    installed materialui

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,10 +1,20 @@
 import React from 'react'
-import './nav.css'
+import './nav.css' 
+import SvgIcon from '@mui/material/SvgIcon';
+
+function HomeIcon(props) {
+  return (
+    <SvgIcon {...props}>
+      <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+    </SvgIcon>
+  );
+}
+
 export default function nav() {
   return (
     <div className='nav'>
         <div>
-            <button>P R O F I L E</button>
+            <HomeIcon color="beige" fontSize="large" />
         </div>
         <div>
             <button>P O S T</button>
