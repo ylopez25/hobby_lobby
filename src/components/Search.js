d1012996f73f84f8ccb6d1d2ed1ed7d6f466e6f1
Yesenia L <ylopezcr123@gmail.com>
2023-09-29 23:54:22 -0400

    removed icons and added words for nav

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,26 +1,26 @@
-import React from 'react'
-import { useState } from 'react';
-import Feed from './Feed';
-import './Search.css'
+import React from "react";
+import { useState } from "react";
+import Feed from "./Feed";
+import "./Search.css";
 
-export default function Search({users}) {
-    const [search,setSearch] = useState('');
+export default function Search({ users }) {
+  const [search, setSearch] = useState("");
 
-    const handleChange = (e) => {
-        const input= e.target.value;
-        setSearch(input)
-    }
-//  let displayUsers = users;
-//     if(search) {
-//         displayUsers = users.filter((user) => {
-//             const {username,city} = user;
-//             const fullUsername = `${username} ${city}`.toLowerCase();
-//             return fullUsername.includes(search.toLowerCase());
-//         })
-//     }
+  const handleChange = (e) => {
+    const input = e.target.value;
+    setSearch(input);
+  };
+  //  let displayUsers = users;
+  //     if(search) {
+  //         displayUsers = users.filter((user) => {
+  //             const {username,city} = user;
+  //             const fullUsername = `${username} ${city}`.toLowerCase();
+  //             return fullUsername.includes(search.toLowerCase());
+  //         })
+  //     }
   return (
-    <div className='search'>
-   <input type= "search" placeholder="Search..." value={search} onChange={handleChange}/>
+    <div className="search">
+      <input type="search" placeholder="Search..." value={search} onChange={handleChange} />
     </div>
-  )
+  );
 }
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,32 +1,20 @@
 import React from "react";
-import logo from "./Your paragraph text.png";
+// import logo from "./Your paragraph text.png";
 import "./nav.css";
-import { createSvgIcon } from "@mui/material/utils";
-import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
-const HomeIcon = createSvgIcon(<path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />, "Home");
-
-const PlusIcon = createSvgIcon(
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-  </svg>,
-  "Plus"
-);
 
 export default function nav() {
   return (
     <div className="nav">
-      <div>
-
-        <img src={logo} style={{ width:"40px" }} alt='logo'/>
+      <div className="nav_left">
+        {/* <img src={logo} style={{ width:"40px" }} alt='logo'/> */}
+        <h3>HL</h3>
+        <p>Inspiration</p>
+        <p>Create</p>
+        <p>Settings</p>
       </div>
-      <div>
-        <HomeIcon fontSize="large" />
-      </div>
-      <div>
-        <PlusIcon fontSize="large" />
-      </div>
-      <div>
-        <ManageAccountsIcon fontSize="large" />{" "}
+      <div className="nav_right">
+        <p>Login</p>
+        <p>Sign up</p>
       </div>
     </div>
   );
