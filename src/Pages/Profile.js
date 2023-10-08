e111a87e8dc733d9da90fa27b3129f5bf782b56a
Yesenia L <ylopezcr123@gmail.com>
2023-10-07 23:48:25 -0400

    created profile divs and placeholders

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -2,6 +2,20 @@ import React from 'react'
 
 export default function Profile() {
   return (
-    <div>Profile</div>
+    <div>
+      <div>
+        <img src="img" alt="img" />
+        <span>name</span>
+        <p>@username</p>
+        <button>Edit Profile</button>
+      </div>
+      <div>
+        <p>filter icon</p>
+        <p>plus icon</p>
+      </div>
+      <div>
+        <img src="1" alt="img1" />
+      </div>
+    </div>
   )
 }
