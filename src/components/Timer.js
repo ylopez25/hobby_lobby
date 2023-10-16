e5c01711d14bc0891c47ddacb24ea3e2ed7dfce6
Yesenia L <ylopezcr123@gmail.com>
2023-10-15 23:57:25 -0400

    progress bar valus

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,13 +1,22 @@
 import React from "react";
-import { CircularProgressbar } from "react-circular-progressbar";
+import { CircularProgressbar, buildStyles} from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
+
 
 export default function Timer() {
   const percentage = 66;
   return (
-    <div style={{ width: 200, height: 200 }}>
+    <div 
+    style={{ width: 200, height: 200 }}>
       Timer
-      <CircularProgressbar value={percentage} text={`${percentage}%`} />
+      <CircularProgressbar
+        value={percentage}
+        // text={minutes + ':' + seconds}
+        styles={buildStyles({
+        textColor:'#fff',
+        // pathColor:mode === 'work' ? red : green,
+        tailColor:'rgba(255,255,255,.2)',
+      })} />
     </div>
   );
 }
