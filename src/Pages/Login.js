92a6d7b62a17c0ebea4c769bdcc03cbff64b10e0
Yesenia L <ylopezcr123@gmail.com>
2023-10-17 18:42:29 -0400

    login page html

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Login() {
   return (
-    <div>Login form</div>
-  )
+    <div className="login template d-flex justify-content-center align-items-center vh-100">
+      <div className="form_container p-5 rounded pg-black">
+        <form>
+          <h3 className="text-center">Sign In</h3>
+          <div className="mb-2">
+            <label htmlFor="email"> Email:</label>
+            <input type="email" placeholder="Enter Email" className="form-control" />
+          </div>
+          <div className="mb-2">
+            <label htmlFor="password">Password:</label>
+            <input type="password" placeholder="Enter Password" className="form-control" />
+          </div>
+          <div className="mb-2">
+            <input type="checkbox" className="custom-control custom-checkbox" id="check" />
+            <label htmlFor="check" className="custom-input-label ms-2">
+              Remember me
+            </label>
+          </div>
+          <div className="d-grid">
+            <button className="btn btn-primary">Sign In</button>
+          </div>
+          <p className="text-end mt-2">
+            Forgot <Link href="/passwordreset">Password?</Link>
+            <Link to="/signup" className="ms-2">
+              Sign up
+            </Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 }
