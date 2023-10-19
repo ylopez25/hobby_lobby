0e8e748f141fcf7220638b5b0eeb39b67037a124
Yesenia L <ylopezcr123@gmail.com>
2023-10-19 17:34:34 -0400

    login useref

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,19 +1,56 @@
 import { Link } from "react-router-dom";
+import { useRef,useState, useEffect } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 
+
 export default function Login() {
+  const userRef = useRef();
+  const errRef= useRef();
+  //create states for pwd and email
+const [pwd, setPwd] = useState('');
+const [user, setUser] = useState('');
+const [err, setErr] = useState('');
+
+  useEffect(() => {
+    userRef.current.focus()
+  }, [])
+
+  useEffect(() => {
+setErr('');
+  }, [user, pwd])
+
   return (
     <div className="login template d-flex justify-content-center align-items-center vh-100">
       <div className="form_container p-5 rounded bg-white">
+        <p
+        ref={errRef}
+        aria-live='assertive'
+        >
+          {err}
+        </p>
         <form>
           <h3 className="text-center">Sign In</h3>
           <div className="mb-2">
-            <label htmlFor="email"> Email:</label>
-            <input type="email" placeholder="Enter Email" className="form-control" />
+            <label htmlFor="username"> Username:</label>
+            <input type="text" 
+            id="username"
+            ref={userRef}
+            autoComplete="off"
+            onChange={(e) => setUser(e.target.value)}
+            value={user}
+            placeholder="Enter Username" 
+            className="form-control"
+            required
+            />
           </div>
           <div className="mb-2">
             <label htmlFor="password">Password:</label>
-            <input type="password" placeholder="Enter Password" className="form-control" />
+            <input type="password" 
+            onChange={(e) => setPwd(e.target.value)}
+            value={pwd}
+            placeholder="Enter Password" 
+            className="form-control" 
+            />
           </div>
           <div className="mb-2">
             <input type="checkbox" className="custom-control custom-checkbox" id="check" />
