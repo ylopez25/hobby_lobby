e62bb3a436f0b7e62adf4244730aa51e3083b777
Yesenia L <ylopezcr123@gmail.com>
2023-10-24 17:00:47 -0400

    profile gallery

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import "../components/Profile.css";
-
+import Loading from '../components/Loading.js';
+import Error from '../components/Error.js';
 import { useState, useEffect } from "react";
 
 export default function Profile() {
   const [users, setUsers] = useState([]);
-  const [allUsers, setAllUsers] = useState([]);
   const [loading, setLoading] = useState(true);
   const [err, setErr] = useState("");
   const [cities, setCities] = useState([]);
-
 
   const API_URL = process.env.REACT_APP_API_URL;
 
@@ -22,14 +21,13 @@ export default function Profile() {
         setLoading(true);
         const res = await fetch(`${API_URL}/v2/users?include=photos`);
         const json = await res.json();
-        console.log(json, "json");
+        // console.log(json, "json");
         const { data, cities, err } = json;
         console.log(data, "data");
         console.log(cities, "cities");
         if (res.ok) {
           setCities(cities);
           setUsers(data);
-          setAllUsers(data);
           setLoading(false);
         } else {
           setErr(err);
@@ -44,30 +42,14 @@ export default function Profile() {
   }, []);
 
   const renderContent = () => {
-    if (users.length === 0) {
-      return `No results for ${search}`;
+    if (loading) {
+      return <Loading />;
+    } else if (err) {
+      return <Error err={err} />;
     } else {
-      return users.map((user) => {
-        const photos = user.photos;
-        return (
-          <div className="userCard">
-  
-            <div className="userinfo">
-              <div className="userinfo_left">
-                <img src={user.pic} alt="img" />
-                <p>{user.user_name}</p>
-              </div>
-            
-            </div>
-          </div>
-        );
-      });
-    }
-  };
-
-  return (
-    <div className="profile">
-      <div className='profile_details'>
+      return (
+        <div className="profile">
+      <div className="profile_details">
         <img src="https://i.pinimg.com/280x280_RS/f2/bc/cf/f2bccfe2d804dd173c3e6f75d3e84ed7.jpg" alt="img" />
         <h1>Yesenia Lopez</h1>
         <p>@username</p>
@@ -75,21 +57,28 @@ export default function Profile() {
       </div>
       <div className="profile_icons">
         <svg>
-        <path d="M9 19.5a1.75 1.75 0 1 1 .001-3.501A1.75 1.75 0 0 1 9 19.5M22.25 16h-8.321c-.724-2.034-2.646-3.5-4.929-3.5S4.795 13.966 4.071 16H1.75a1.75 1.75 0 0 0 0 3.5h2.321C4.795 21.534 6.717 23 9 23s4.205-1.466 4.929-3.5h8.321a1.75 1.75 0 0 0 0-3.5M15 4.5a1.75 1.75 0 1 1-.001 3.501A1.75 1.75 0 0 1 15 4.5M1.75 8h8.321c.724 2.034 2.646 3.5 4.929 3.5s4.205-1.466 4.929-3.5h2.321a1.75 1.75 0 0 0 0-3.5h-2.321C19.205 2.466 17.283 1 15 1s-4.205 1.466-4.929 3.5H1.75a1.75 1.75 0 0 0 0 3.5"></path>
+          <path d="M9 19.5a1.75 1.75 0 1 1 .001-3.501A1.75 1.75 0 0 1 9 19.5M22.25 16h-8.321c-.724-2.034-2.646-3.5-4.929-3.5S4.795 13.966 4.071 16H1.75a1.75 1.75 0 0 0 0 3.5h2.321C4.795 21.534 6.717 23 9 23s4.205-1.466 4.929-3.5h8.321a1.75 1.75 0 0 0 0-3.5M15 4.5a1.75 1.75 0 1 1-.001 3.501A1.75 1.75 0 0 1 15 4.5M1.75 8h8.321c.724 2.034 2.646 3.5 4.929 3.5s4.205-1.466 4.929-3.5h2.321a1.75 1.75 0 0 0 0-3.5h-2.321C19.205 2.466 17.283 1 15 1s-4.205 1.466-4.929 3.5H1.75a1.75 1.75 0 0 0 0 3.5"></path>
         </svg>
-        <svg >
+        <svg>
           <Link to="/create">
-          <path d="M22 10h-8V2a2 2 0 0 0-4 0v8H2a2 2 0 0 0 0 4h8v8a2 2 0 0 0 4 0v-8h8a2 2 0 0 0 0-4"></path>
+            <path d="M22 10h-8V2a2 2 0 0 0-4 0v8H2a2 2 0 0 0 0 4h8v8a2 2 0 0 0 4 0v-8h8a2 2 0 0 0 0-4"></path>
           </Link>
         </svg>
       </div>
       <div className="profile_gallery">
-        <img src="1" alt="img1" />
-        <img src="1" alt="img1" />
-        <img src="1" alt="img1" />
-        <img src="1" alt="img1" />
+        {users[0]?.photos.map((item) => {
+          return <img src={item?.photo} alt="Profile_gallery" />;
+        })}
       </div>
-      {renderContent()}
     </div>
-  )
+      );
+    }
+  };
+
+
+  return (
+    <div>
+   {renderContent()}
+    </div>
+  );
 }
