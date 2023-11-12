ee7922dc97338593a7c0322e807efd2ef80e3f19
Yesenia L <ylopezcr123@gmail.com>
2023-11-11 23:41:18 -0500

    setup for landing page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import '../components/Home.css'
+import React from "react";
+import "../components/Home.css";
 
 export default function Home() {
   return (
     <div className="home">
+      <h2>Welcome to Hobby Lobby!</h2>
+      <button>Explore Now</button>
     </div>
-  )
+  );
 }
