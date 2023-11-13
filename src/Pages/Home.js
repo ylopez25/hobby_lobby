7af191049af012a995fe712d650dd2c27522c434
Yesenia L <ylopezcr123@gmail.com>
2023-11-13 17:53:58 -0500

    benefits section added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -13,12 +13,26 @@ export default function Home() {
         <p>Become your best self with limitless possibilities</p>
         <br />
         <button>
-          <Link to="/users">Join Now</Link>
+          <Link to="/signup">Join Now</Link>
         </button>
         <button>Learn more</button>
       </div>
       <div className="home_info">
         <img src={LandingImg} alt="" />
+      </div>
+      <div className="home_benefits">
+        <div>
+          <h3>Benefit 1</h3>
+          <p>Get inspired and inspire</p>
+        </div>
+        <div>
+          <h3>Benefit 2</h3>
+          <p>Increase productivity and decrease distractions</p>
+        </div>
+      <div>
+        <h3>Benefit 3</h3>
+        <p>Become your best self</p>
+      </div>
       </div>
     </div>
   );
