2fcf032569ceaa9828f15094a9e4a1fc625801bd
Yesenia L <ylopezcr123@gmail.com>
2023-12-13 23:56:47 -0500

    not working code

--- src/Pages/EditProfile.js
+++ src/Pages/EditProfile.js
@@ -1,59 +1,62 @@
 import React from "react";
-import axios from "axios";
+//import axios from "axios";
 import "../components/EditProfile.css";
-import { useState, useEffect } from "react";
+//import { useState, useEffect } from "react";
 //import { useHistory } from "react-router-dom";
 import { useNavigate, useParams, Link } from "react-router-dom";
 
 const API_URL = process.env.REACT_APP_API_URL;
 
-export default function EditProfile() {
-  const [users, setUsers] = useState([]);
-  const [cities, setCities] = useState([]);
+export default function EditProfile(props) {
+
+  const usersss = props.location.state.userss;
+  console.log(usersss, 'props?')
+  // const [users, setUsers] = useState([]);
+  // const [cities, setCities] = useState([]);
   // let history = useHistory();
-  const navigate = useNavigate();
-  let { id } = useParams();
-  let userInfo = {
-    pic: '',
-    userName : '',
-    name: '',
-    city: '',
-  }
+  // const navigate = useNavigate();
+  // let { id } = useParams();
+  // let userInfo = {
+  //   pic: '',
+  //   userName : '',
+  //   name: '',
+  //   city: '',
+  // }
 
-  const updateProfile = (info, id) => {
-    try {
-      axios.put(`${API_URL}/v2/users/${id}`, info).then((response) => {
-        const neuVar = [...users, ...cities];
-        const index = neuVar.findIndex((user) => user.id === Number(id));
-        neuVar[index] = info;
-        setUsers(neuVar);
-        navigate(`/users/${id}`);
-        // history.push(`/users/${id}`);
-      });
-    } catch (err) {
-      console.log("err", err);
-    }
-  };
+  // const updateProfile = (info, id) => {
+  //   try {
+  //     axios.put(`${API_URL}/v2/users/${id}`, info).then((response) => {
+  //       const neuVar = [...users, ...cities];
+  //       const index = neuVar.findIndex((user) => user.id === Number(id));
+  //       neuVar[index] = info;
+  //       //setUsers(neuVar);
+  //       navigate(`/users/${id}`);
+  //       // history.push(`/users/${id}`);
+  //     });
+  //   } catch (err) {
+  //     console.log("err", err);
+  //   }
+  // };
 
 
-  const handleChange = (e) => {
-    setUsers(e.target.value);
-    setCities(e.target.value)
-  }
+  // const handleChange = (e) => {
+  //  // setUsers(e.target.value);
+  //   setCities(e.target.value)
+  // }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    updateProfile(id);
-  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   updateProfile(id);
+  // };
 
   return (
     <div className="d-flex justify-content-center align-items-center vh-100 bg-black">
       <div className="form_container p-5 rounded text-white mb-1 text-center">
-        <form onSubmit={handleSubmit}>
+        <form >
           <h3>Edit Profile Info:</h3>
           <div>
             <label htmlFor="new_img">Profile Picture:</label>
-            <input type="text" className="form-control" onChange={handleChange}/>
+            <input type="text" className="form-control" />
           </div>
           <div>
             <label htmlFor="new_username">Username:</label>
